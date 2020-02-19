7e90cec17cb2933b39abf0c6e87385099bdcc9a7
TheWaterFell <59457095+TheWaterFell@users.noreply.github.com>
2020-02-19 02:11:43 -0500

    feat: added C1 and C2 processed data

--- pkg_vtt_extract/f_visualization.py
+++ pkg_vtt_extract/f_visualization.py
@@ -6,7 +6,7 @@ import matplotlib.ticker as ticker
 from .e_agg_df_count import group_by_person_episode, count_per_episode
 
 
-def lineplot_per_ep (org_names, matt_nomatt):
+def lineplot_per_ep (org_names, matt_nomatt, C1_C2):
     person_episode_group = group_by_person_episode(org_names)
     if matt_nomatt == 'nomatt':
         person_episode_group = person_episode_group.loc[person_episode_group['person'] != 'matt']
@@ -16,12 +16,14 @@ def lineplot_per_ep (org_names, matt_nomatt):
     fig, ax = pyp.subplots(nrows=1, ncols=1, figsize=(30, 5))
     ax = sns.lineplot(x="episode", y="time_count", 
         data=person_episode_group, hue='person', marker='o', ax=ax)
-
-    ax.set(xlim=(1, 90))
+    if C1_C2 == 'C1':
+        ax.set(xlim=(1, 115))
+    else:
+        ax.set(xlim=(1, 90))
     ax.legend(bbox_to_anchor=(1, 1))
     ax
 
-def lineplot_cont_count (org_names, matt_nomatt):
+def lineplot_cont_count (org_names, matt_nomatt, C1_C2):
     person_episode_group = group_by_person_episode(org_names)
     count_per_ep = count_per_episode(person_episode_group)
     if matt_nomatt == 'nomatt':
@@ -33,6 +35,10 @@ def lineplot_cont_count (org_names, matt_nomatt):
     ax = sns.lineplot(x="episode", y="cont_time_count", 
         data=count_per_ep, hue='person', ax=ax)
 
-    ax.set(xlim=(1, 90))
+    if C1_C2 == 'C1':
+        ax.set(xlim=(1, 115))
+    else:
+        ax.set(xlim=(1, 90))
+
     ax.legend(bbox_to_anchor=(1, 1))
     ax
